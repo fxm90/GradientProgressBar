2af34aadd68a2f43d1d6aff4bbfc818c1f83e437
Felix Mau <f.mau@mytaxi.com>
2017-08-26 17:27:11 +0200

    * added basic tests * refactored extension for uicolor initializer

---
+++ GradientProgressBarTests/Extensions/CGRect/CGRect+AdjustmentsTests.swift
@@ -0,0 +1,37 @@
+//
+//  CGRect+AdjustmentsTests.swift
+//  CGRect+AdjustmentsTests
+//
+//  Created by Felix Mau on 04.03.17.
+//  Copyright © 2017 Felix Mau. All rights reserved.
+//
+
+import XCTest
+@testable import GradientProgressBar
+
+class CGRectAdjustmentsTests: XCTestCase {
+
+    // "Float.ulpOfOne" - The positive difference between 1.0 and the next greater representable number.
+    // Source: https://developer.apple.com/documentation/swift/float/2886872-ulpofone
+    let accuracy = CGFloat(Float.ulpOfOne)
+
+    func testUpdateWidthBy10Percent() {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 123.4)
+        let rectWidthUpdateBy10Percent = rect.updateWidth(byPercentage: 1.1)
+
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.origin.x, 0.0, accuracy: accuracy)
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.origin.y, 0.0, accuracy: accuracy)
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.width, 110.0, accuracy: accuracy)
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.height, 123.4, accuracy: accuracy)
+    }
+
+    func testUpdateWidthByMinus10Percent() {
+        let rect = CGRect(x: 0.0, y: 0.0, width: 100.0, height: 123.4)
+        let rectWidthUpdateBy10Percent = rect.updateWidth(byPercentage: 0.9)
+
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.origin.x, 0.0, accuracy: accuracy)
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.origin.y, 0.0, accuracy: accuracy)
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.width, 90.0, accuracy: accuracy)
+        XCTAssertEqualWithAccuracy(rectWidthUpdateBy10Percent.height, 123.4, accuracy: accuracy)
+    }
+}

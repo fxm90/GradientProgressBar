e10461e2af3c1367faec9ccafdd690904d4c4123
Felix Mau <me@felix.hamburg>
2018-12-23 13:20:06 +0100

    ✨ :: Added extension to array of type `UIColor`, to get corresponding `cgColor`

---
+++ Example/Tests/Extensions/Array/ArrayCGColorsTestCase.swift
@@ -0,0 +1,29 @@
+//
+//  ArrayCGColorsTestCase.swift
+//  GradientProgressBar_Tests
+//
+//  Created by Felix Mau on 12/23/18.
+//  Copyright © 2018 Felix Mau. All rights reserved.
+//
+
+import XCTest
+
+@testable import GradientProgressBar
+
+class ArrayCGColorsTestCase: XCTestCase {
+    func testArrayShouldContainCorrectCGColors() {
+        // Given
+        let uiColors: [UIColor] = [.red, .yellow, .green]
+
+        // When
+        let cgColors = uiColors.cgColors
+
+        // Then
+        for (index, uiColor) in uiColors.enumerated() {
+            let expectedCGColor = uiColor.cgColor
+            let receivedCGColor = cgColors[index]
+
+            XCTAssertEqual(expectedCGColor, receivedCGColor)
+        }
+    }
+}

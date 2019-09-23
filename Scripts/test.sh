28199e36912273a76c30640c126d9899fa8cd58b
Felix Mau <me@felix.hamburg>
2019-09-23 08:12:01 +0200

    ♻️ :: update build script for xCode 11 and iOS 13

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -16,5 +16,5 @@ xcodebuild \
   -workspace GradientProgressBar.xcworkspace \
   -scheme GradientProgressBar-Example \
   -sdk iphonesimulator \
-  -destination 'platform=iOS Simulator,name=iPhone XR,OS=12.2' \
+  -destination 'platform=iOS Simulator,name=iPhone 11 Pro,OS=13.0' \
   build test | xcpretty && exit ${PIPESTATUS[0]}
